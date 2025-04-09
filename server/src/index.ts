311c86c7f449a5227f0c27f1149fc5322c26e121
Fercali777 <fernandocalixto89@gmail.com>
2025-04-09 16:12:05 +0200

    fixed change server for work with typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
+// Importación de dependencias
+import express, { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+// Cargar variables de entorno
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,18 +15,18 @@ app.use(cors());
 app.use(express.json());
 
 // Ruta de prueba
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Servidor funcionando 🚀');
 });
 
 // Conectar a MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI as string) // TypeScript no sabe si MONGO_URI está en el .env, entonces lo tratamos como string
   .then(() => {
     console.log(' Conectado a MongoDB Atlas');
     app.listen(PORT, () => {
       console.log(` Servidor corriendo en http://localhost:${PORT}`);
     });
   })
-  .catch((err) => {
+  .catch((err: any) => {
     console.error(' Error al conectar a MongoDB:', err);
   });
