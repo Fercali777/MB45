daa5779d6a2e6f4a7b09d2aa8d0c4f86e98712c3
Fercali777 <fernandocalixto89@gmail.com>
2025-05-26 15:37:57 +0200

    For lukas

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,26 +1,27 @@
-import { Strategy as JwtStrategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
-import passport from 'passport';
-import dotenv from 'dotenv';
+// import { Strategy as JwtStrategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
+// import passport from 'passport';
+// import dotenv from 'dotenv';
+// import {User} from '../models/User';
 
-dotenv.config();
+// dotenv.config();
 
-const opts: StrategyOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET as string, 
-};
+// const opts: StrategyOptions = {
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//   secretOrKey: process.env.JWT_SECRET as string, 
+// };
 
-passport.use(
-  new JwtStrategy(opts, async (jwt_payload: any, done: any) => {
-    try {
-      const user = await User.findById(jwt_payload.id);
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    } catch (error) {
-      return done(error, false);
-    }
-  })
-);
+// passport.use(
+//   new JwtStrategy(opts, async (jwt_payload: any, done: any) => {
+//     try {
+//       const user = await User.findById(jwt_payload.id);
+//       if (user) {
+//         return done(null, user);
+//       }
+//       return done(null, false);
+//     } catch (error) {
+//       return done(error, false);
+//     }
+//   })
+// );
 
-export default passport;
+// export default passport;
