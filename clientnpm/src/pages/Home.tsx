0b56f397f89c5b64ff9db7bf21f85bfca9c0ca4e
fercali777 <fernandocalixto89@gmail.com>
2025-08-03 16:46:14 +0200

    changes in home

--- clientnpm/src/pages/Home.tsx
+++ clientnpm/src/pages/Home.tsx
@@ -30,10 +30,10 @@ const slides = [
   (
     <div className="slider-contentMB flex">
       <div className="slider-textMB">
-        <h1 className="font-white">Elegant Storage</h1>
+<h1 className="font-white">The Soul of the Table</h1>
         <hr/>
-        <h3 className="font-white">Berlin Cabinet</h3>
-        <p className="font-white">Keep your space organized with the Berlin Cabinet, featuring minimalist design and ample storage.</p>
+        <h3 className="font-white">Kyoto Dining Table</h3>
+        <p className="font-white">Minimalist dining table inspired by Japanese design, made of rich walnut wood with a matte finish.</p>
         <a href="/products/6833579bd886a4f7f4fff2a7" className="button-1 bt-black" title="Product Details">SEE MORE</a>
       </div>
       <img className="product-picture-sliderMB" alt="Berlin Cabinet" src="https://res.cloudinary.com/deqsrgxeu/image/upload/v1748195225/tyjsefrtcmunwp7ubied.png" />
